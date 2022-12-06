e53e166977a165d28dfdbd18c71660595116ac2f
RVT <99425881+Hren352@users.noreply.github.com>
2022-12-06 19:46:07 +0300

    Update 1stinstall.sh

--- 1stinstall.sh
+++ 1stinstall.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/bash
 
+echo "update ..."
 sudo apt update -y
+echo "update ......"
 sudo apt upgrade -y
+echo "installing MC"
 sudo apt install mc -y
+echo "installing NCDU"
 sudo apt install ncdu -y
+echo "installing fail2ban"
 sudo apt install fail2ban -y
+echo "installing iptstate"
 sudo apt install iptstate -y
 
+echo "setup timezone"
 timedatectl set-timezone Europe/Moscow
 
+echo "setup Docker"
 #Установка Docker в Ubuntu
 sudo apt install docker -y
 sudo apt install docker.io -y
@@ -16,6 +24,7 @@ sudo usermod -aG docker $USER
 sudo usermod -aG docker ${USER}
 
 #Установка Docker Compose на Linux
+echo "setup Docker Compose"
 sudo apt install curl -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 #Для установки другой версии замените 1.29.2 на нужный вам номер.
@@ -32,3 +41,9 @@ docker-compose --version
 #VSCODE
 #docker
 #remote-containers
+
+#cleaning system
+sudo apt-get autoclean
+sudo apt-get autoremove
+sudo apt-get clean
+sudo apt-get -f install
